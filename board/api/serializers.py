5ab612bf5f8f6cca183ffe7f32d5869f3bca44f4
svalee <triksrimer@gmail.com>
2017-11-02 12:09:05 +0200

    post submiting asynchronously

--- board/api/serializers.py
+++ board/api/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from board.models import Post
 
+
 class PostCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('title', 'text', 'name', 'email', 'image')
+        fields = ('title', 'text', 'name', 'email', 'image', 'id')
+        read_only_fields = ('id',)
